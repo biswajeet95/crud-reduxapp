2340e40f776f614c5b8bdfff5226f0600908f589
bmoharana95 <bmoharana95@gmail.com>
2023-03-07 21:59:21 +0530

    design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addUser, deleteUser,updateUsername } from "./features/Users";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [newEmail, setNewEmail] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addUser">
+        <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)} />
+        <input type="email" placeholder='email...' onChange={(e) => setEmail(e.target.value)} />
+        <button onClick={() => {
+          dispatch(
+            addUser({
+              id: userList[userList.length - 1].id + 1,
+              name,
+              email,
+            })
+          )
+        }}
+        >Add User</button>
+      </div>
+      <div className='displayUsers'>
+        {userList.map((user) => {
+          return (
+            <div key={user.id}>
+              <h1 >{user.name}</h1>
+              <h1 >{user.email}</h1>
+              <input text="text"
+                placeholder='New Username...'
+                onChange={(e) => {
+                 setNewEmail(e.target.value)
+                }} />
+              <button onClick={() => dispatch(
+                updateUsername({id:user.id , email: newEmail})
+
+              )}>Update name</button>
+              <button onClick={() => { dispatch(deleteUser({ id: user.id })); }}>Delete</button>
+            </div>
+
+          )
+        })}
+      </div>
+
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      email: "Bret",
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      email: "Antonette",
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+      email: "clementine",
+    },
+    {
+      id: 4,
+      name: "Patricia Lebsack",
+      email: "Karianne",
+    },
+    {
+      id: 5,
+      name: "Chelsey Dietrich",
+      email: "Kamren",
+    },
+    {
+      id: 6,
+      name: "Jake Runolfsdottir V",
+      email: "Leopoldo_Corkery",
+    },
+    {
+      id: 7,
+      name: "Mrs. Dennis Schulist",
+      email: "Karley_Dach",
+    },
+    {
+      id: 8,
+      name: "Kurtis Weissnat",
+      email: "Elwyn.Skiles",
+    },
+  ];
